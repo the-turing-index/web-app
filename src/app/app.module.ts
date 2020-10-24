2441775cda87c53f4b8d1dc32ca7b00e8fd6a8b8
Joshua Sevy <joshuasevy@outlook.com>
2020-10-23 20:48:01 -0600

    Add heading component and boilerplate to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { HeadingComponent } from './heading/heading.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    HeadingComponent
   ],
   imports: [
     BrowserModule
