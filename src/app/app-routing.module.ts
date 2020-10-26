9283589f972b45d4dfd731bd579ada7d2c08f50e
Sean Leas <phoenix_24leas.com>
2020-10-25 20:22:33 -0600

    Add 404 component  resolves #20
    Co-authored-by: Josh Sevy <56780211+JoshSevy@users.noreply.github.com>
    Co-authored-by: Nicole Latifi<59676637+NicoleLatifi@users.noreply.github.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AboutComponent } from './about/about.component';
+import { MapComponent } from './map/map.component'
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
+  { path: '', redirectTo: '/map', pathMatch: 'full' },
+  { path: 'map', component: MapComponent },
   { path: 'about', component: AboutComponent },
-  
+  { path: '**', component: PageNotFoundComponent }
 ]
 
 @NgModule({
