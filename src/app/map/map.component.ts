b3ec2dfb619995a5bf61bd6646d4ab15a4120b0f
Joshua Sevy <joshuasevy@outlook.com>
2020-10-27 14:01:53 -0600

    Add mock data file for dynamic rendering

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Component({
@@ -10,14 +11,32 @@ import { HttpClient } from '@angular/common/http';
 export class MapComponent implements OnInit {
 
   constructor(private http: HttpClient) {
+    loadedData = [];
 
    }
 
   ngOnInit(): void {
+    this.fetchData();
   }
 
-  fetchData() {
-    this.http.get('url')
+  onFetchData() {
+    this.fetchData();
   }
 
+  private fetchData() {
+    this.http
+      .get('url')
+      .pipe(map((responseData: {[key: string]: }) => {
+        const dataArray = [];
+        for (const key in  responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            dataArray.push({ ...responseData[key] , id: key })
+          }
+        }
+        return dataArray;
+      }))
+      .subscribe(response => {
+        console.log(response);
+    });
+  }
 }
---
+++ src/shared/mockData.ts
@@ -0,0 +1,52 @@
+const data = {
+        id: null,
+        type: 'calendars',
+        attributes: [
+            {
+                mod1: {
+                    frontend: {
+                        zoom_link: ''
+                    },
+                    backend: {
+                        zoom_link: ''
+                    }
+                }
+            },
+            {
+                mod2: {
+                    frontend: {
+                        zoom_link: ''
+                    },
+                    backend: {
+                        zoom_link: ''
+                    }
+                }
+            },
+            {
+                mod3: {
+                    frontend: {
+                        zoom_link: ''
+                    },
+                    backend: {
+                        zoom_link: ''
+                    }
+                }
+            },
+            {
+                mod4: {
+                    frontend: {
+                        zoom_link: ''
+                    },
+                    backend: {
+                        zoom_link: ''
+                    }
+                }
+            },
+            {
+                community: {
+                    zoom_link: ''
+                }
+            }
+        ]
+    }
+}
