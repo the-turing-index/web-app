606c7fef149292f03bc14c91b1c9ee2eddb7de01
joshua sevy <joshuasevy@joshuas-MacBook-Pro.local>
2020-11-04 21:47:06 -0700

    Add date time to modals for display
    Co-authored-by: Linus Leas <22572676+Leasw144@users.noreply.github.com>
    Co-authored-by: Nicole Latifi <59676637+NicoleLatifi@users.noreply.github.com>

--- src/shared/DemoData.ts
+++ src/shared/DemoData.ts
@@ -1,6 +1,9 @@
 export const frontEndMod1 = {
   title: 'JS Fun @ The Library ',
   description: 'In front end web development, the programming language you will encounter most often is JavaScript. Soon, we’ll also use HTML and CSS to help a user interact with our applications, however before we do that, we need to understand how to use JavaScript to handle the logic of our applications.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '9:30 a.m.',
+  end: '11:00 a.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://frontend.turing.io/projects/module-1/library.html'
 };
@@ -8,6 +11,9 @@ export const frontEndMod1 = {
 export const frontEndMod2 = {
   title: 'Objects: Inheritance',
   description: 'What is the easiest way to be wealthy with programming? Inheritance.Inheritance is an important concept in object oriented programming. In the classical inheritance, methods from base class get copied into derived class. What is the easiest way to be wealthy with programming? Inheritance.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '9:30 a.m.',
+  end: '11:00 a.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://frontend.turing.io/lessons/module-2/oop-3-inheritance.html'
 };
@@ -15,6 +21,9 @@ export const frontEndMod2 = {
 export const frontEndMod3 = {
   title: 'React Router 5',
   description: 'React Router is a library that allows us to make our single page React applications mimic the behavior of multipage apps. It provides the ability to use browser history, allowing users to navigate with forward / back buttons to specific views of the app. Most modern sites use some form of routing. React Router exposes this functionality through a series of components.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '9:30 a.m.',
+  end: '11:00 a.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://frontend.turing.io/lessons/module-3/react-router-v5.html'
 };
@@ -22,6 +31,9 @@ export const frontEndMod3 = {
 export const backEndMod1 = {
   title: 'Memory Allocation',
   description: 'In some ways, computer memory is better than human memory. When the computer memorizes something, it won’t forget it until we tell it to. You’ve never tried to open a saved document on your computer and it told you that it forgot it. Humans on the other hand forget things all the time.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '12:30 p.m.',
+  end: '3:00 p.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://backend.turing.io/module1/lessons/memory_allocation'
 };
@@ -29,6 +41,9 @@ export const backEndMod1 = {
 export const backEndMod2 = {
   title: 'Restful Routes and CRUD',
   description: 'The model must be able to Create, Read, Update, and Delete resources. Computer scientists often refer to these functions by the acronym CRUD. A model should have the ability to perform at most these four functions in order to be complete. If an action cannot be described by one of these four operations, then it should potentially be a model of its own.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '9:30 a.m.',
+  end: '11:00 a.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://backend.turing.io/module2/lessons/restful_routes_and_crud'
 };
@@ -36,6 +51,9 @@ export const backEndMod2 = {
 export const backEndMod3 = {
   title: 'Building an API',
   description: 'REST, as a concept, came from the need to mimic some amount of “state” between HTTP requests and responses, since HTTP is a stateless protocol. It ties together the idea of HTTP verbs and URI paths/routes into a uniform interface by which we can state “I want to create a new resource” or “I want to change something about a specific resource”.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '11:00 a.m.',
+  end: '12:00 p.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://backend.turing.io/module3/lessons/building_an_api'
 };
@@ -43,6 +61,9 @@ export const backEndMod3 = {
 export const module4 = {
   title: 'Recursion',
   description: 'Recursion is a technique for iterating over an operation by having a function call itself repeatedly until it arrives at a result. Most loops can be rewritten in a recursive style, and in some functional languages this approach to looping is the default.',
+  date: 'Friday, Nov. 6th 2020',
+  start: '1:30 p.m.',
+  end: '3:30 p.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://mod4.turing.io/lessons/cs/recursion.html'
 };
@@ -50,6 +71,9 @@ export const module4 = {
 export const community = {
   title: 'Guest Speaker: Gary Flowers ',
   description: 'Over 25 years of experience in the technology industry, Gary has focused on enabling business process and innovation from a Senior and Executive level vantage point. Gary has worked in multiple industries both domestic and globally to drive increased profit and process efficiency. ',
+  date: 'Friday, Nov. 6th 2020',
+  start: '11:00 a.m.',
+  end: '12:00 p.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://www.linkedin.com/in/garyflowers/'
 };
@@ -57,6 +81,9 @@ export const community = {
 export const kitchen = {
   title: 'Lets get Social',
   description: 'Need a break? Need someone to bounce ideas off and collaborate with? Come to the kitchen to connect with fellow students and staff!',
+  date: 'Friday, Nov. 6th 2020',
+  start: '9:00 a.m.',
+  end: '9:00 p.m.',
   zoomLink: 'https://turingschool.zoom.us/j/96910748883',
   lessonPlan: 'https://turing.io/'
 };
