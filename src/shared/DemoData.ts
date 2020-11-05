146a1d49b442ff9ec20561f677f2f1123408bbe4
joshua sevy <joshuasevy@joshuas-MacBook-Pro.local>
2020-11-04 17:29:51 -0700

    Created demoData for app to make better user experience
    Co-authored-by: Nicole Latifi <59676637+NicoleLatifi@users.noreply.github.com>
    Co-authored-by: Linus Leas <22572676+Leasw144@users.noreply.github.com>

---
+++ src/shared/DemoData.ts
@@ -0,0 +1,62 @@
+export const frontEndMod1 = {
+  title: 'JS Fun @ The Library ',
+  description: 'In front end web development, the programming language you will encounter most often is JavaScript. Soon, we’ll also use HTML and CSS to help a user interact with our applications, however before we do that, we need to understand how to use JavaScript to handle the logic of our applications.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://frontend.turing.io/projects/module-1/library.html'
+};
+
+export const frontEndMod2 = {
+  title: 'Objects: Inheritance',
+  description: 'What is the easiest way to be wealthy with programming? Inheritance.Inheritance is an important concept in object oriented programming. In the classical inheritance, methods from base class get copied into derived class. What is the easiest way to be wealthy with programming? Inheritance.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://frontend.turing.io/lessons/module-2/oop-3-inheritance.html'
+};
+
+export const frontEndMod3 = {
+  title: 'React Router 5',
+  description: 'React Router is a library that allows us to make our single page React applications mimic the behavior of multipage apps. It provides the ability to use browser history, allowing users to navigate with forward / back buttons to specific views of the app. Most modern sites use some form of routing. React Router exposes this functionality through a series of components.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://frontend.turing.io/lessons/module-3/react-router-v5.html'
+};
+
+export const backEndMod1 = {
+  title: 'Memory Allocation',
+  description: 'In some ways, computer memory is better than human memory. When the computer memorizes something, it won’t forget it until we tell it to. You’ve never tried to open a saved document on your computer and it told you that it forgot it. Humans on the other hand forget things all the time.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://backend.turing.io/module1/lessons/memory_allocation'
+};
+
+export const backEndMod2 = {
+  title: 'Restful Routes and CRUD',
+  description: 'The model must be able to Create, Read, Update, and Delete resources. Computer scientists often refer to these functions by the acronym CRUD. A model should have the ability to perform at most these four functions in order to be complete. If an action cannot be described by one of these four operations, then it should potentially be a model of its own.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://backend.turing.io/module2/lessons/restful_routes_and_crud'
+};
+
+export const backEndMod3 = {
+  title: 'Building an API',
+  description: 'REST, as a concept, came from the need to mimic some amount of “state” between HTTP requests and responses, since HTTP is a stateless protocol. It ties together the idea of HTTP verbs and URI paths/routes into a uniform interface by which we can state “I want to create a new resource” or “I want to change something about a specific resource”.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://backend.turing.io/module3/lessons/building_an_api'
+};
+
+export const module4 = {
+  title: 'Recursion',
+  description: 'Recursion is a technique for iterating over an operation by having a function call itself repeatedly until it arrives at a result. Most loops can be rewritten in a recursive style, and in some functional languages this approach to looping is the default.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://mod4.turing.io/lessons/cs/recursion.html'
+};
+
+export const community = {
+  title: 'Guest Speaker: Gary Flowers ',
+  description: 'Over 25 years of experience in the technology industry, Gary has focused on enabling business process and innovation from a Senior and Executive level vantage point. Gary has worked in multiple industries both domestic and globally to drive increased profit and process efficiency. ',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://www.linkedin.com/in/garyflowers/'
+};
+
+export const kitchen = {
+  title: 'Lets get Social',
+  description: 'Need a break, need someone to bounce your crazy ideas off. Come to the kithen and connect with fellow students, and staff. No Idea is too crazy here.',
+  zoomLink: 'https://turingschool.zoom.us/j/96910748883',
+  lessonPlan: 'https://turing.io/'
+};
